d1705bf993c8bf055167336404eec864a7df3bca
febuniac <febuniac@gmail.com>
2015-03-30 09:45:02 -0300

    Espaços das letras funcionando

--- Teste1.py
+++ Teste1.py
@@ -9,98 +9,16 @@ import turtle
 import random
 #Importa a biblioteca do turtle e do random
 #import tkinter (funçao que pergunta se quer recomeçar)
-space = '_ ' 
+space = '_____   ' 
 #definição dos tracinhos das palavras
+#Importa a biblioteca do turtle
 janela = turtle.Screen() 
 #Define e importa a janela da biblioteca do turtle
 janela.bgcolor("orange") 
 #Definição da cor da janela
 janela.title("Jogo da Forca")
 #Define o nome da janela do turtle
-def cabeca():   
-        poste = turtle.Turtle()          #Constroi a cabeca
-        poste.speed(8)
-        poste.pensize(10)
-        poste.penup()
-        poste.setpos(-180,105)
-        poste.left(180)
-        poste.pendown()
-        poste.circle(20)
-        poste.color("black")
-
-def tronco():     
-    
-        poste = turtle.Turtle()      #Constroi o Tronco
-        poste.speed(8)
-        poste.pensize(10)
-        poste.penup()
-        poste.setpos(-180,65)
-        poste.pendown()
-        poste.right(90)
-        poste.forward(100)
-        poste.color('black')
-        
-
-def braco_esquerdo():
-    
-    poste=turtle.Turtle()         #Constroi o braço esquerdo
-    poste.speed(8)
-    poste.pensize(10)
-    poste.penup()
-    poste.setpos(-180, 35)
-    poste.pendown()
-    poste.right(135)
-    poste.forward(40)
-    poste.color("black")
-    
-
-def braco_direito():
-    
-    poste=turtle.Turtle()         #Constroi o braço direito
-    poste.speed(8)
-    poste.pensize(10)
-    poste.penup()
-    poste.setpos(-180, 35)
-    poste.pendown()
-    poste.right(400)
-    poste.forward(40)
-    poste.color("black")
-    
-
-
-def perna_direita():
-    
-    poste=turtle.Turtle()     #Constroi a perna direita
-    poste.speed(8)
-    poste.pensize(10)
-    poste.penup()
-    poste.setpos(-180,-35)
-    poste.pendown()
-    poste.right(40)
-    poste.forward(40)
-    poste.color("black")
-    
-    
-
-    
-def perna_esquerda():
-    
-    poste=turtle.Turtle()     #Constroi a perna esquerda
-    poste.speed(8)
-    poste.pensize(10)
-    poste.penup()
-    poste.setpos(-180,-35)
-    poste.pendown()
-    poste.left(-140)
-    poste.forward(40)
-    poste.color("black")
-
-
-
-
-
-
-poste = turtle.Turtle()                               
+poste = turtle.Turtle()
 #Definição do que define o lapis ( A própia tartaruga)
 poste.pensize(15)
 #Define a grossura do meu lapis
@@ -131,27 +49,120 @@ poste.forward(30)
 arquivo = open("palavras_forca.txt", encoding='utf-8')
 #Abre o arquivo (palavras_forca em formato text0(txt))e como ele esta codificado(utf-8)
 ler=arquivo.readlines()
+#Define ler como ler as linhas do arquivo
+numero = random.randint(0, len(ler))
+#escolhe palavras de forma aleatória de acordo com a posição na lista
+print(ler[numero])
+#print a palvra escolhida(a palavra é escolhida como um numero da lista)
+def cabeca():   
+        poste = turtle.Turtle()          #Constroi a cabeca
+        poste.speed(8)
+        poste.pensize(10)
+        poste.penup()
+        poste.setpos(-180,105)
+        poste.left(180)
+        poste.pendown()
+        poste.circle(20)
+        poste.color("black")
+
+
+
+def tronco():
+        poste = turtle.Turtle()
+        poste.speed(8) # Constroi o Tronco 
+        poste.pensize(10)
+        poste.penup()
+        poste.setpos(-180,65)
+        poste.pendown()
+        poste.right(90)
+        poste.forward(100)
+        poste.color('black')
+        
+
+
+
+def braco_esquerdo():
+    
+    poste=turtle.Turtle()         #Constroi o braço esquerdo
+    poste.speed(8)
+    poste.pensize(10)
+    poste.penup()
+    poste.setpos(-180, 35)
+    poste.pendown()
+    poste.right(135)
+    poste.forward(40)
+    poste.color("black")
+    
+braco_esquerdo()
+def braco_direito():
+    
+    poste=turtle.Turtle()         #Constroi o braço direito
+    poste.speed(8)
+    poste.pensize(10)
+    poste.penup()
+    poste.setpos(-180, 35)
+    poste.pendown()
+    poste.right(400)
+    poste.forward(40)
+    poste.color("black")
+    
+
+def perna_direita():
+    
+    poste=turtle.Turtle()
+    poste.speed(8)
+    poste.pensize(10)
+    poste.penup()
+    poste.setpos(-180,-35)
+    poste.pendown()
+    poste.right(40)
+    poste.forward(40)
+    poste.color("black")
+    
+    
+perna_direita()
+    
+def perna_esquerda():
+    
+    poste=turtle.Turtle()
+    poste.speed(8)
+    poste.pensize(10)
+    poste.penup()
+    poste.setpos(-180,-35)
+    poste.pendown()
+    poste.left(-140)
+    poste.forward(40)
+    poste.color("black")
+
+
+arquivo = open("palavras_forca.txt", encoding='utf-8')
+#Abre o arquivo (palavras_forca em formato text0(txt))e como ele esta codificado(utf-8)
+ler=arquivo.readlines()
 
 #Define ler como ler as linhas do arquivo
+numero = random.randint(0, len(ler))
+#escolhe palavras de forma aleatória de acordo com a posição na lista
+#comprimento = len(arquivo)
 
-
+poste.penup()
+poste.setpos(5,-60)
+poste.write(len(ler[numero])*space)
+#Tira os erros da frase.
+print(ler[numero])
+#print a palvra escolhida(a palavra é escolhida como um numero da lista)
 lista_limpa = []
 for x in ler:
     y = x.strip()
     if y != "":
         lista_limpa.append(y)
 ler = lista_limpa
-numero = random.randint(0, len(ler))
-#escolhe palavras de forma aleatória de acordo com a posição na lista
-#comprimento = len(arquivo)
 poste.penup()
 poste.setpos(5,-60)
-poste.write(len(ler[numero])*space,font=("Monaco",30,"normal"))
+poste.write(len(ler[numero])*space)
 #Tira os erros da frase.
 print(ler[numero])
 #print a palvra escolhida(a palavra é escolhida como um numero da lista)
-
-
+lista_limpa = []
 errado = 0
 certo = 0
 while errado<6 and certo<len(ler[numero]):
@@ -160,8 +171,8 @@ while errado<6 and certo<len(ler[numero]):
     for i in range(len(ler[numero])):
         if caixa == ler[numero][i]:
             print(i)
-            poste.setpos(5 + 36*(i),-60)
-            poste.write(caixa,font=("Monaco",30,"normal"))
+            poste.setpos(5 + 20*(i),-60)
+            poste.write(caixa,font=("Arial",30,"normal"))
     #i é a posicao da letra correta    
     # i é uma variavel que aumenta de 1 em 1 (i comeca em zero e se for menor que o tamanhao da palavra vai aumentando de 1 em 1 )
     if caixa not in ler[numero]:
@@ -179,7 +190,7 @@ while errado<6 and certo<len(ler[numero]):
         elif errado == 6:
             perna_esquerda()
     if errado==6:
-        poste.write('O jogo acabou! ENFORCADO!!', align="center",font=("Monaco",30) )
+        poste.write('O jogo acabou! ENFORCADO!!', align="center",font=("Arial",30) )
         break
             
  #from random import randint
@@ -209,5 +220,11 @@ while errado<6 and certo<len(ler[numero]):
 
 janela.exitonclick()
 
+arquivo = open('entrada.csv','r')
+leitura=arquivo.read(1000)
+lista = leitura.split(',')
+import random
+palavra = random.choice(lista)
+print(palavra)
 
 
